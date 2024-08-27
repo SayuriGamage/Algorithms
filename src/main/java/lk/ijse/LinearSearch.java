fdfb9d07f75aab543d40abbe7a94ed0e74ebf921
Sayuri Gamage <sayuriyashodhara@gmail.com>
2024-08-27 15:04:21 +0530

    case1

--- src/main/java/lk/ijse/LinearSearch.java
+++ src/main/java/lk/ijse/LinearSearch.java
@@ -1,2 +1,28 @@
-package lk.ijse;public class LinearSearch {
+package lk.ijse;
+
+public class LinearSearch {
+
+
+
+    public static void main(String[] args) {
+
+        int[] arr={3,2,5,8,7};
+        int taget=5;
+        int result=searchNUmber(arr,taget);
+
+        if (result!=-1){
+            System.out.println("Element found at index: "+result);
+        }else {
+            System.out.println("Element is not found");
+        }
+    }
+
+    private static int searchNUmber(int[] arr, int taget) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == taget) {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
